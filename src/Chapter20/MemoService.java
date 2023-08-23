3e47215afcd2d0e8655f8a23231a21934edd0f46
ESKim <you@example.com>
2023-08-23 12:38:13 +0900

    add MemoService.java

---
+++ src/Chapter20/MemoService.java
@@ -0,0 +1,26 @@
+package Chapter20;
+
+import java.util.List;
+
+public class MemoService {
+
+    public List<MemoVO> selectMemos() {
+        return null;
+    }
+
+    public MemoVO selectMemo(int selectNo) {
+        return null;
+    }
+
+    public int insertMemo(MemoVO vo) {
+        return 0;
+    }
+
+    public int updateMemo(MemoVO vo) {
+        return 0;
+    }
+
+    public int deleteMemo(int deleteNo){
+        return 0;
+    }
+}
